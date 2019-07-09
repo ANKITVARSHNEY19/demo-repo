1db11300280585dd840971790c8dcf2c174042bd
BhushanLal <bhushanlal972@gmail.com>
2019-07-09 16:05:52 +0530

    Index file updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 #This controller is used for testing the jenkins works fine or not
 class UsersController < ApplicationController
-  skip_before_action :authenticate_request #, only: [:register, :authenticate]
+  skip_before_action :authenticate_request, only: [:register, :login]
 
  def login
    command = AuthenticateUser.call(params[:email], params[:password])
     user = User.find_by(email: params[:email])
    if command.success?
-     render json: {status: 200, data: {auth_token: command.result, user_details: user}, msg: "You are login successfully." }
+     render json: {status: 200, auth_token: command.result, user_details: user, msg: "You are login successfully." }
    else
-     render json: { error: command.errors }, status: :unauthorized
+     render json: {status: 422, msg: command.errors.values.join(',') }, status: :unprocessable_entity
    end
  end
 
@@ -19,13 +19,13 @@ class UsersController < ApplicationController
     user = User.new(name: params[:name], email: params[:email], password: params[:password])
     user.save!
     token = AuthenticateUser.call(params[:email], params[:password])
-    render json: {status: 200, data: {auth_token: token.result, user_details: user}, msg: "You are registered successfully."}
+    render json: {status: 200, auth_token: token.result, user_details: user, msg: "You are registered successfully."}
   rescue => e
-    render json: {status: 401, data: {auth_token: nil, user_details: nil}, msg: e.message}, status: :unauthorized
+    render json: {status: 401, auth_token: nil, user_details: nil, msg: e.message}, status: :unprocessable_entity
   end  
  end
 
- def questions_list  
+ def questions_list
     #render json: {status: 200, questions: Quiz.all,  msg: "Success."}
      render json: {status: 200, questions: Quiz.all.map{|que| [id: que.id, question: que.question, options: [que.opt1,que.opt2,que.opt3,que.opt4], answer: que.answer]}.flatten,  msg: "Success."}
  end
